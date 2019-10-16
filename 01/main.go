5bad5902c011c1d358e79f8066f7c2ffd9bf2b62
Anthony <orlovskikh@gmail.com>
2019-10-16 16:28:59 +0300

    level 4

--- 01/main.go
+++ 01/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+var x int = 42
+var y string = "James Bond"
+var z bool = true
+
+type antor int
+
+var a antor
+
 func main() {
 	fmt.Println("Hello")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
+	fmt.Println(s)
+	s = fmt.Sprintf("%v", a)
+	fmt.Println(s)
 }
