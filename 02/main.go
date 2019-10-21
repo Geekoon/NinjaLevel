222b793032a41747ffa53f36949d389fd27f292b
Anthony <orlovskikh@gmail.com>
2019-10-21 16:34:49 +0300

    Ninja Level 2. Ex.1-3

--- 02/main.go
+++ 02/main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
-func main () {
-	fmt.Println("Hello")
+const (
+	utc = 67
+	tc int = 23
+)
+
+var a int
+
+func main() {
+	a = 35
+	fmt.Printf("%b\n", a)
+	fmt.Printf("%d\n", a)
+	fmt.Printf("%#x\n", a)
+	fmt.Println(utc)
+	fmt.Println(tc)
 }
